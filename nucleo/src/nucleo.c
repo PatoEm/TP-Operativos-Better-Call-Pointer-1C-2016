effa947c952504adec14df79b52de91d5f0715b0
PatoEm <moletpatricio@hotmail.com>
2016-04-17 22:46:35 -0300

    archivo source en nucleo

---
+++ nucleo/src/nucleo.c
@@ -0,0 +1,16 @@
+/*
+ * nucleo.c
+ *
+ *  Created on: 17/4/2016
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Esto es el fucking nucleo"); /* prints !!!Hello World!!! */
+	return 0;
+}
+
