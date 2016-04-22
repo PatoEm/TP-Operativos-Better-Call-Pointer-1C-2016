6900ab61b4dac204e2cff26064f07e56e0f4beea
nicohova <nicohova.95@hotmail.com>
2016-04-22 00:28:22 -0300

    commons y shared library creadas

--- shared-library/Debug/subdir.mk
+++ shared-library/Debug/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../cpu.c 
+../shared.c 
 
 OBJS += \
-./cpu.o 
+./shared.o 
 
 C_DEPS += \
-./cpu.d 
+./shared.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/shared-library" -I"/home/utnso/workspace/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
---
+++ shared-library/shared.c
@@ -0,0 +1,8 @@
+/*
+ * shared.c
+ *
+ *  Created on: 22/4/2016
+ *      Author: utnso
+ */
+
+
