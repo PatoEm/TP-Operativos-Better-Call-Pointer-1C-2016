0009b8641e2041cd930526fb1a16c786c994a3a9
EmilianoCaggiano <emiliano_river27@hotmail.com>
2016-05-06 23:48:51 -0300

    Me rompen los huevos las cosas mal identadas

--- consola/src/mainConsola.c
+++ consola/src/mainConsola.c
@@ -25,67 +25,67 @@ int main(void) {
 
 	char* direccionDeArchivo;
 	printf("Ingrese archivo: ");
-    scanf("%s",direccionDeArchivo);
+    	scanf("%s",direccionDeArchivo);
 
-    //Obtengo el tamaño y contenido del archivo
-    leerProgramaAnsisop(direccionDeArchivo);
+    	//Obtengo el tamaño y contenido del archivo
+    	leerProgramaAnsisop(direccionDeArchivo);
 		//puts("hola");
 
 
 	//Envio la longitud del archivo
-    enviarMensaje(fdSocketConsola, tamanio, sizeof(long), 0);
+	enviarMensaje(fdSocketConsola, tamanio, sizeof(long), 0);
 
 
-			//Envio el contenido del archivo
-    enviarMensaje(fdSocketConsola, buffer, tamanio+1);
+	//Envio el contenido del archivo
+    	enviarMensaje(fdSocketConsola, buffer, tamanio+1);
 
 
-			int operacionId;
-			int exit = false;
-			while(!exit){
-				//Esperando operacion a realizar
-				int estado = recibirMensaje(fdSocketConsola, operacionId, sizeof(int));
+	int operacionId;
+	int exit = false;
+	while(!exit){
+	//Esperando operacion a realizar
+	int estado = recibirMensaje(fdSocketConsola, operacionId, sizeof(int));
 
 
-				if(!estado){
-					puts("Nucleo desconectado");
-					exit = true;
-				}else{
-					switch(operacionId){
-						int estadoOpe;
+	if(!estado){
+		puts("Nucleo desconectado");
+		exit = true;
+	}else{
+		switch(operacionId){
+		int estadoOpe;
 
-						/*case IMPRIMIR: ;
+		/*case IMPRIMIR: ;
 
-							char* variable;
-							int valorDeVariable;
+			char* variable;
+			int valorDeVariable;
 
-							estadoOpe= recibirMensaje(fdSocketConsola, &variable, sizeof(char*));
-							estadoOpe = recibirMensaje(fdSocketConsola, valorDeVariable, sizeof(int));
+			estadoOpe= recibirMensaje(fdSocketConsola, &variable, sizeof(char*));
+			estadoOpe = recibirMensaje(fdSocketConsola, valorDeVariable, sizeof(int));
 
-							for(int i =0; i< ; i++){
-							if(estadoOpe) printf("VARIABLE: %d \n%d \n"", variable,valorDeLaVariable);
-							}
-									break; */ //ESTO NOSE MUY BIEN COMO SE HACE
-						case IMPRIMIRTEXTO: ;
-
-							long longitudDelTexto;
-							estadoOpe = recibirMensaje(fdSocketConsola, longitudDelTexto, sizeof(long)); //recivo la longitud
-
-							if(estadoOpe){
-								char* texto = malloc(sizeof(char)*(longitudDelTexto));
-								estadoOpe= recv(fdSocketConsola, &texto, sizeof(char)*(longitudDelTexto), 0);//recivo el texto
-
-								if(estadoOpe) printf("IMPRESION DE TEXTO: %s \n",texto);
-
-								free(texto);
-							}
-							break;
-					}
-				}
+			for(int i =0; i< ; i++){
+				if(estadoOpe) printf("VARIABLE: %d \n%d \n"", variable,valorDeLaVariable);
 			}
-			close(fdSocketConsola);	// Cierro el socket
+		break; */ //ESTO NOSE MUY BIEN COMO SE HACE
+		case IMPRIMIRTEXTO: ;
 
-			return EXIT_SUCCESS;
+			long longitudDelTexto;
+			estadoOpe = recibirMensaje(fdSocketConsola, longitudDelTexto, sizeof(long)); //recivo la longitud
+
+			if(estadoOpe){
+				char* texto = malloc(sizeof(char)*(longitudDelTexto));
+				estadoOpe= recv(fdSocketConsola, &texto, sizeof(char)*(longitudDelTexto), 0);//recivo el texto
+
+				if(estadoOpe) printf("IMPRESION DE TEXTO: %s \n",texto);
+
+				free(texto);
+					}
+		break;
+		}
+	}
+	}
+	close(fdSocketConsola);	// Cierro el socket
+
+return EXIT_SUCCESS;
 }
 
 
