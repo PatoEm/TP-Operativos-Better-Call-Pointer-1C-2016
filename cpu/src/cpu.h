5df9beca84296079c40a82482d67abf149a59257
Sebas <sebas.esperanza@gmail.com>
2016-05-29 20:05:44 -0300

    Primer commit al nuevo repo, subo prototipos de funciones del Parser, para armar un diccionario y manejar las funciones poliformicamente al momento de ejecutarlas

--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -8,6 +8,9 @@
 #ifndef CPU_H_
 #define CPU_H_
 
+/*******************************************************
+ * DEPENDENCIAS
+ *******************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/config.h>
@@ -18,12 +21,99 @@
 #include <libreriasCompartidas/socket.h>
 #define FAIL -1
 
-//variables
+
+/*******************************************************
+ * VARIABLES GLOBALES
+ *******************************************************/
 char* ipUMC;
 char* umcPort;
 char* nucleoPort;
 char* ipNucleo;
 int clienteNucleo;
 
+
+/*******************************************************
+ * TIPOS DE DATOS
+ *******************************************************/
+typedef struct functionOperators {
+	char op[2];
+	void* (*func)(void*, void*);
+} FunctionOperators;
+
+
+/*******************************************************
+ * FUNCIONES AnSISOP
+ *******************************************************/
+
+/*
+ * definirVariable
+ */
+t_puntero definirVariable(t_nombre_variable identificador_variable);
+
+/*
+ * obtenerPosicionVariable
+ */
+t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable);
+
+/*
+ * dereferenciar
+ */
+t_valor_variable dereferenciar(t_puntero direccion_variable);
+
+/*
+ * asignar
+ */
+void asignar(t_puntero direccion_variable, t_valor_variable valor);
+
+/*
+ * obtenerValorCompartida
+ */
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
+
+/*
+ * 	asignarValorCompartida
+ */
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
+
+/*
+ * 	irAlLabel
+ */
+t_puntero_instruccion irAlLabel(t_nombre_etiqueta etiqueta);
+
+/*
+ * llamarConRetorno
+ */
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+
+/*
+ * retornar
+ *
+ */
+t_puntero_instruccion retornar(t_valor_variable retorno);
+
+/*
+ * imprimir
+ */
+int imprimir(t_valor_variable valor_mostrar);
+
+/*
+ * imprimirTexto
+ */
+int imprimirTexto(char* texto);
+
+/*
+ * entradaSalida
+ */
+int entradaSalida(t_nombre_dispositivo dispositivo, int tiempo);
+
+/*
+ * wait
+ */
+int wait(t_nombre_semaforo identificador_semaforo);
+
+/*
+ * signal
+ */
+int signal(t_nombre_semaforo identificador_semaforo);
 
 #endif /* CPU_H_ */
