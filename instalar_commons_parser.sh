2518ca7111ff6bd167751ecaf00a8f3a14b9ab86
Santiago <s.calgaro.martin@gmail.com>
2016-06-23 15:34:01 -0300

    Script para instalar commons y parser en un paso

---
+++ instalar_commons_parser.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+echo ">>> Ingrese contraseña:"
+sudo echo ">>> Gracias."
+
+# Instalación de utnso commons
+cd ~
+echo ">>> Git init"
+git init
+echo ">>> Clonando commons"
+git clone "https://github.com/sisoputnfrba/so-commons-library"
+cd so-commons-library
+echo ">>> Instalando commons"
+sudo make all
+sudo make install
+
+# Instalación del parser
+cd ~
+echo ">>> Clonando parser"
+git clone "https://github.com/sisoputnfrba/ansisop-parser"
+cd ansisop-parser
+cd parser
+echo ">>> Instalando parser"
+sudo make all
+sudo make install
+
+echo ">>> Probando parser"
+cd ~
+cd ansisop-parser
+cd so-ansisop-parser-dummy
+sudo make all
+./build/so-ansisop-parser-dummy
+
+echo ">>> FIN"
