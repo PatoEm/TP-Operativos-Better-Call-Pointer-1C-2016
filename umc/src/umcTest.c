decba705d1fc3da226060d22be4f108d4efb31ed
nicohova <nicohova.95@hotmail.com>
2016-07-02 00:32:50 -0300

    Hola II

---
+++ umc/src/umcTest.c
@@ -0,0 +1,22 @@
+/*
+ * umcTest.c
+ *
+ *  Created on: 2/7/2016
+ *      Author: utnso
+ */
+
+/*
+ * umcTest.c
+ *
+ *  Created on: 2/7/2016
+ *      Author: utnso
+ */
+
+#include "umc.h"
+
+int main(int argc, char ** argv){
+
+
+
+	return 0;
+}
