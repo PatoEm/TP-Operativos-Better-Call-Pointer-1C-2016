98becc38f7dc1231b52250291bba46bd39427b68
Sebastián <SebastianEsperanza@users.noreply.github.com>
2016-07-19 14:32:39 -0300

    Update mainSwap.c

--- swap/src/mainSwap.c
+++ swap/src/mainSwap.c
@@ -1,13 +1,17 @@
 #include "swap.h"
 
+t_log* logger = NULL;
+
 int main(void) {
-	swaplog = log_create("swap.log", "SWAP", TRUE, LOG_LEVEL_INFO);
+
 	leerArchivoDeConfiguracion("/home/utnso/tp-2016-1c-Better-call-pointer/swap/swapconfig");
 	tamPaginaLoca=atoi(tamPagina);
 	iniciarEstructurasSwap();
 	archivoMappeado = crearArchivo(tamArchivo, nombreSwap);
-
+	
 	manejoDeConexiones();
-
+	
+	log_destroy(logger);
+	
 	return EXIT_SUCCESS;
 }
