3c3d0a5a9a8d00efba6396ce802e93472e5f53f0
Seba <sebas.esperanza@gmail.com>
2016-07-25 00:38:47 -0300

    Agrego un .sh para levantar los configs del  test 4

--- el levantador de modulos/prueba_4/levantarNucleo.sh
+++ el levantador de modulos/prueba_4/levantarNucleo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~/tp-2016-1c-Better-call-pointer/nucleo/ #Me muevo al directorio para que me mande todos los logs ahi y no los deja en el directorio del tp
-./Debug/nucleo /home/utnso/tp-2016-1c-Better-call-pointer/nucleo/confignucleo
+./Debug/nucleo /home/utnso/tp-2016-1c-Better-call-pointer/nucleo/confignucleo4
