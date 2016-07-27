d3309ded95f5bdc891bb4a16281df886b68d6402
Seba <sebas.esperanza@gmail.com>
2016-07-27 01:12:43 -0300

    esperarConfirmacion al terminar el quantum

--- cpu/src/mainCpu.c
+++ cpu/src/mainCpu.c
@@ -138,6 +138,9 @@ int main() {
 					return FALSE;
 				}
 				log_info(getLogger(), "Termine el quantum");
+
+				esperarConfirmacion(socketNucleo);
+
 				free(pcbProceso);
 			}
 			seguirEjecutando = TRUE;
